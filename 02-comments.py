b52006e69661d6b51b2cd733953fc47dc29deaff
albecabrera <albe.cabrera@yahoo.de>
2025-04-06 21:40:37 +0200

    first change

---
+++ 02-comments.py
@@ -0,0 +1,7 @@
+# Esto es un comentario de una sola línea
+'''
+Esto es un comentario de varias líneas
+que puede abarcar varias líneas.
+También puede incluir caracteres especiales como comillas simples ' o dobles ".
+'''
+
---
+++ 03-variables.py
@@ -0,0 +1,13 @@
+saludo = "Hola Mundo 2"
+print(saludo)
+# Variables
+# Variables son espacios en memoria que almacenan datos
+# Se pueden crear variables de diferentes tipos
+# Tipos de variables
+
+name = "Alberto"
+lastname = "Gonzalez"
+age = 30
+print("Alberto")
+print("lastname")
+print("age")

bc9215157dddcb16bbb8f8a7286f020697b16e82
albecabrera <albe.cabrera@yahoo.de>
2025-04-09 21:59:16 +0200

    cambio conversiones

---
+++ 09-inmutable.py
@@ -0,0 +1,6 @@
+# ¿Qué es inmutabilidad? Significa que no se puede cambiar el valor de una variable después de haber sido asignada.
+
+nombre = "Ricardo" 
+print(nombre)
+edad = 47
+print(edad)
---
+++ 10-convertions.py
@@ -0,0 +1,11 @@
+
+value = 100
+type_value = type(value)
+print(value)
+print(type_value)  # <class 'int'>
+
+# Convertir un valor a otro tipo
+# int  a str
+value = str(value)
+print(value)  # 100
+print(type(value))  # <class 'str'>
